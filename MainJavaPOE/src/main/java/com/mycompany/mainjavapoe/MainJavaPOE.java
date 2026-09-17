17b93c6dc5f9d28780988ff1e0e8965ae4ffc84d
ST10514005 <ST10514005@rcconnect.edu.za>
2026-09-17 11:18:31 +0200

    Update MainJavaPOE.java

--- MainJavaPOE/src/main/java/com/mycompany/mainjavapoe/MainJavaPOE.java
+++ MainJavaPOE/src/main/java/com/mycompany/mainjavapoe/MainJavaPOE.java
@@ -19,8 +19,10 @@ public class MainJavaPOE {
         String name;
         String surname;
         String userName;
-        String cellNum;
+        String logUsername;
         String passWord;
+        String logPassword;
+        String cellNum;
         char character;
           
         System.out.print("Enter name :" + "");
@@ -32,39 +34,79 @@ public class MainJavaPOE {
         System.out.print("Enter cell phone number :" + "");
         cellNum = myInput.next();
         
-        checkCellPhoneNumber(cellNum);
- 
-        
         System.out.print("Create a username :" + "");
         userName = myInput.next();
-        checkUserName(userName); 
-         
-            System.out.print("Create a password :" + " ");
-            passWord = myInput.next();
-            checkPasswordComplexity(passWord);
-        
-    }
-    public static String checkUserName(String userName) 
-    {
-        Scanner Scanner = new Scanner(System.in);
-        
-        while (!(userName.length()<= 5 && userName.contains("_"))) {
-            
-         System.out.println("Username is not correctlty formatted");
-         System.out.println("Please ensure username contains an underscore and is no more five characters");
-         
-         System.out.print("Re-enter username");
-         userName = Scanner.nextLine();
-            }
-        System.out.println("Username successfully captured");
-        return "Username is correct";
-            
+ 
+        System.out.print("Create a password :" + " ");
+        passWord = myInput.next();
+   
+        //Validation for registration (username)
+        while (!(userName.length() <= 5 && userName.contains(""))) {
+      
+                  System.out.println("Username incorrectly informatted");
+                  System.out.println("Username must: ");
+                  System.out.println("Be no more 5 characters long");
+                  System.out.println("Contain an underscore");
+                  System.out.println("Re-enter username: ");
+                  userName = myInput.next();
         }
+                  System.out.println("Username successfully captured");
+                  
+                  //Validation for registration (password)
+                  while (!(checkPasswordComplexity(passWord))) {
+              
+              
+                  System.out.println("Password is not correctly formatted");
+                  System.out.println("Password must: ");
+                  System.out.println("Be at least 8 characters long");
+                  System.out.println("Contain at least one capital letter");
+                  System.out.println("Contain atleast one number");
+                  System.out.println("Contain at least one special character");
+              
+              System.out.print("Re-enter password: ");
+              passWord = myInput.next();
+                  
+                  }
+                  loginClass user = new loginClass(name, surname, userName, passWord, cellNum);
+                  
+                  // Login Validation
+                  System.out.println("/n---Login---");
+                  System.out.println("Enter username");
+                  logUsername = myInput.next();
+                  System.out.println("Enter passWord");
+                  logPassword = myInput.next();
+                  
+                  
+          
+          System.out.println("Password successfully captured");
+           
+          //Validation for cell phone number on Registration
+          while (!(checkCellPhoneNumber(cellNum))){
+              System.out.println("Cellphone number is incorrectly formatted");
+              System.out.println("Cell phone number must contain international country code");
+              
+              System.out.println("Re-enter cell phone number");
+              cellNum = myInput.nextLine();
+          }
+                  
+    }
+    public static boolean checkUserName(String userName)  {
+        
+     
+          if(userName.length() <= 5 && userName.contains("_")) {
+              System.out.println("Username successfully captured"); 
+              
+          } else {
+              System.out.println("Username is not correctly formatted");
+          }
+          return true;
+      }
+    
+      
+    
         
      public static boolean checkPasswordComplexity(String passWord) {
    
-        while (true) {
-           
             boolean hasUppercase = false;
             boolean hasNumber = false;
             boolean hasSpecial = false;
@@ -87,7 +129,7 @@ public class MainJavaPOE {
                     hasSpecial = true;
                 }
                 }
-            if (hasUppercase && hasNumber && hasSpecial) {
+            if (passWord.length() >=8 && hasUppercase && hasNumber && hasSpecial) {
                 System.out.println("Password successfully captured");
                 return true;
             }
@@ -96,9 +138,7 @@ public class MainJavaPOE {
                 System.out.println ("Please ensure that the password contains at least eight characters, a capital letter, a number and a special character");
                 return false;
             }
-            }
-   
-            }
+     }
       public static boolean checkCellPhoneNumber(String cellNum) {
              
             while (true) {
@@ -106,20 +146,20 @@ public class MainJavaPOE {
                 String countryCode = "+27";
                 boolean hasCountryCode = false;
                 
-                if (cellNum.length() <= 10 && hasCountryCode ) {
+                if (cellNum.startsWith("+27") && cellNum.length() == 12) {
                     System.out.println("Cell phone number is successfully added");
                     return true;
                     
                 } 
                 else {
                     System.out.println("Cellphone number is incorrect or does not contain international code");
-                    
+                    System.out.println("Please ensure that the number starts with +27 and contains 9 digits after +27");
                     return false;
                 }
             } 
             }
+      
+      
+  }
+
      
-         
-    
-    
-}
