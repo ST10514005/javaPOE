ee68786649a8cfa162cf4b0775a9569381198d0b
ST10514005 <ST10514005@rcconnect.edu.za>
2026-09-17 11:18:50 +0200

    Update loginClass.java

--- MainJavaPOE/src/main/java/com/mycompany/mainjavapoe/loginClass.java
+++ MainJavaPOE/src/main/java/com/mycompany/mainjavapoe/loginClass.java
@@ -5,77 +5,109 @@
 package com.mycompany.mainjavapoe;
 
 import java.util.Scanner;
+ 
 /**
  *
  * @author ntoko
  */
 public class loginClass {
-        
-    public static boolean loginUser() {
-        //Declarations of variables 
-        String logUserName;
+    
+        public static void loginClass() {
+             //Declarations
+        String name;
+        String surname;
         String userName;
-        String regUserName;
-        String logPassWord;
+        String logUsername;
         String passWord;
-        String regPassWord;
+        String logPassword;
+        String cellNum;
         
-        while(true) {
-    Scanner myInput = new Scanner(System.in);
+            
+            Scanner myInput = new Scanner(System.in);
+            
+            System.out.print("Enter username");
+            logUsername = myInput.nextLine();
+            
+            System.out.print("Enter password");
+            logPassword = myInput.nextLine();
+            
+            //Comparing the Strings
+            if (logUsername.equals(MainJavaPOE.checkUserName(userName)) && logPassword.equals(MainJavaPOE.checkPasswordComplexity(passWord))) {
+                
+                System.out.println("Login successful!");
+        } else {
+                System.out.print("Login is unsuccessful");
+            }
+        }
     
-    System.out.println("Please enter your username");
-    logUserName = myInput.next();
-    
-    System.out.println("Enter your password");
-    logPassWord = myInput.next();
-    
-    if (validateUsername(logUserName) && validatePassWord(logPassWord)) {
-        
-        System.out.println("Login successful!");
-              return true;
-    } else {
-        
-        System.out.println("Username or passed incorrect.");
-        System.out.println("please try again.");
-        
-    }
-   
-    }
-    }
-    
-    public static boolean validateUsername(String logUserName) {
+    public static boolean checkUserName(String userName)  {
         
      
-        while(true){
-        String regUserName; 
-    if(regUserName.equals(logUserName)) {
-        System.out.println("Username matches");
-        return true;
-    }else {
-        System.out.println("Username incorrect,please try again");
-        return false;
-    }
+          if(userName.length() <= 5 && userName.contains("_")) {
+              System.out.println("Username successfully captured"); 
+              
+          } else {
+              System.out.println("Username is not correctly formatted");
+          }
+          return true;
+      }
     
-    }
+      
     
-    }
-    
-    public static boolean validatePassWord (String logPassWord) {
-        //Declarations
-        String regPassWord;
-        String logPassWord;
-           while(true){
-         
-    if(regPassWord.equals(logPassWord)) {
-        System.out.println("Password matches");
-        return true;
         
-    }else {
-        System.out.println("Password incorrect,please try again");
-        return false;
-    }
-        
-    }
-    }
+     public static boolean checkPasswordComplexity(String passWord) {
+   
+            boolean hasUppercase = false;
+            boolean hasNumber = false;
+            boolean hasSpecial = false;
+            
+            for (int i = 0; i < passWord.length(); i++) {
+                
+                 char character;
+                 
+                character = passWord.charAt(i);
+                
+                if (Character.isUpperCase(character)) {
+                    hasUppercase = true;
+                } 
+                
+                if (Character.isDigit(character)) {
+                    hasNumber = true;
+                }
+                
+                if (!Character.isLetterOrDigit(character)) {
+                    hasSpecial = true;
+                }
+                }
+            if (passWord.length() >=8 && hasUppercase && hasNumber && hasSpecial) {
+                System.out.println("Password successfully captured");
+                return true;
+            }
+            else {
+                System.out.println("Password is not correctly formatted");
+                System.out.println ("Please ensure that the password contains at least eight characters, a capital letter, a number and a special character");
+                return false;
+            }
+     }
+      public static boolean checkCellPhoneNumber(String cellNum) {
+             
+            while (true) {
+      
+                String countryCode = "+27";
+                boolean hasCountryCode = false;
+                
+                if (cellNum.startsWith("+27") && cellNum.length() == 12) {
+                    System.out.println("Cell phone number is successfully added");
+                    return true;
+                    
+                } 
+                else {
+                    System.out.println("Cellphone number is incorrect or does not contain international code");
+                    System.out.println("Please ensure that the number starts with +27 and contains 9 digits after +27");
+                    return false;
+                }
+            } 
+            }
+      
+    
 }
-
