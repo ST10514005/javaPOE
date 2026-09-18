cc6ff2101486ddc9f3835e21ef1b373802ad2547
ST10514005 <ST10514005@rcconnect.edu.za>
2026-09-18 13:36:13 +0200

    Update MainJavaPOE.java
    Added comments to my code

--- MainJavaPOE/src/main/java/com/mycompany/mainjavapoe/MainJavaPOE.java
+++ MainJavaPOE/src/main/java/com/mycompany/mainjavapoe/MainJavaPOE.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
  */
 public class MainJavaPOE {
 
+    //The method below prompts the user for details and stores them
     public static void main(String[] args) {
         Scanner myInput = new Scanner(System.in);
 
@@ -21,6 +22,7 @@ public class MainJavaPOE {
         System.out.print("Enter surname: ");
         String surname = myInput.next();
 
+        //validation  for userName
         System.out.print("Create a username: ");
         String userName = myInput.next();
         while (!loginClass.checkUserName(userName)) {
@@ -33,6 +35,7 @@ public class MainJavaPOE {
         }
         System.out.println("Username successfully captured");
 
+        //validation for password
         System.out.print("Create a password: ");
         String passWord = myInput.next();
         while (!loginClass.checkPasswordComplexity(passWord)) {
@@ -47,6 +50,7 @@ public class MainJavaPOE {
         }
         System.out.println("Password successfully captured");
 
+        //validation for phone number
         System.out.print("Enter cell phone number: ");
         String cellNum = myInput.next();
         while (!loginClass.checkCellPhoneNumber(cellNum)) {
@@ -57,6 +61,8 @@ public class MainJavaPOE {
         }
         System.out.println("Cell phone number successfully captured");
 
+        //Below is the login phase where login details are needed which is username & password
+        //Line 66 is basically linking the login class with the variable inputs
         loginClass user = new loginClass(name, surname, userName, passWord, cellNum);
 
         System.out.println("\n---Login---");
@@ -65,6 +71,8 @@ public class MainJavaPOE {
         System.out.print("Enter password: ");
         String logPassword = myInput.next();
 
+        //Line 74 checks whether username & password are correct, recalls whether the login wprked and 
+        //diplay appropriate login message
         boolean isLoggedIn = user.loginUser(logUsername, logPassword);
         System.out.println(user.returnLoginStatus(isLoggedIn));
     }
